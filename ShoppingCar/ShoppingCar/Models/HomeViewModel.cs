ea9a41da2c6d4387e2ff88f7516f1725db45123d
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-06-26 17:36:30 -0500

    Añadir productos al carrito finalizado

---
+++ ShoppingCar/ShoppingCar/Models/HomeViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace ShoppingCar.Models {
+    public class HomeViewModel {
+        public ICollection<ProductsHomeViewModel> Products { get; set; }
+        public float Quantity { get; set; }
+    }
+}
